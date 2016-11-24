c623fc5a4d10f579b8319b8ffed0f24c853f79fc
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-11-24 16:27:21 -0500

    X's and O's can be added to board

---
+++ Desktop/bitmaker/projects/ticTacToe/README.md
@@ -0,0 +1,4 @@
+How to approach:
+
+- state all the cases that a possible for each win that apply to both classes
+of X's and O's
---
+++ Desktop/bitmaker/projects/ticTacToe/interactive.js
@@ -0,0 +1,14 @@
+$(function() {
+  var turn = 0;
+ $('td').on('click', function() {
+
+   if (turn % 2 === 0 ) {
+
+  $(this).html('X');
+   }else {
+     $(this).html('O');
+   }
+    
+   turn++
+ });
+});
